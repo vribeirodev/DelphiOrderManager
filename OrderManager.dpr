9a09a949fd370a2e032ea7749ca0727a6ae3ca2b
Vinicius Ribeiro <vribeiro.dev@gmail.com>
2023-05-24 10:31:23 -0300

    Criado projeto

---
+++ OrderManager.dpr
@@ -0,0 +1,14 @@
+program OrderManager;
+
+uses
+  Vcl.Forms,
+  FormLogin in 'FormLogin.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
